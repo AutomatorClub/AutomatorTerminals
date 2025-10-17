68b37ec90ff474cf749179cd55a49b2d8a71b378
Mareks Pikalovs <mareks.pikalovs@gmail.com>
2025-10-17 11:49:26 +0000

    Added Unix Toolbox

--- tools/10-install_uv.sh
+++ tools/10-install_uv.sh
@@ -8,5 +8,12 @@ if [[ "${ID}" != "debian" ]]; then
 fi
 
 # Install uv
-curl -LsSf https://astral.sh/uv/install.sh -o /tmp/install_uv.sh | bash
-source $HOME/.local/bin/env
+curl -LsSf https://astral.sh/uv/install.sh -o /tmp/install_uv.sh
+cat /tmp/install_uv.sh | bash
+# source $HOME/.local/bin/env
+
+# To enable shell autocompletion for uv commands, run one of the following:
+echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
+echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
+# echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+# echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc
