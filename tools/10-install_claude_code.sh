9fe36f4453c5d22ce154cfa7101b94138bd13aeb
Mareks Pikalovs <mareks.pikalovs@gmail.com>
2025-10-17 14:28:37 +0000

    Note on using custom coding agent

--- tools/10-install_claude_code.sh
+++ tools/10-install_claude_code.sh
@@ -12,3 +12,10 @@ curl -fsSL https://claude.ai/install.sh | bash
 
 # Install latest version
 # curl -fsSL https://claude.ai/install.sh | bash -s latest
+
+# use custom coding agent https://www.youtube.com/watch?v=l1uA7pJZoRM
+# echo 'export ANTHROPIC_DEFAULT_OPUS_MODEL="glm-4.5-air"' >> ~/.zshrc
+# echo 'export ANTHROPIC_DEFAULT_SONNET_MODEL="glm-4.6"' >> ~/.zshrc
+# echo 'export ANTHROPIC_DEFAULT_HAIKU_MODEL="glm-4.6"' >> ~/.zshrc
+# echo 'export CLAUDE_CODE_SUBAGENT_MODEL="glm-4.5-air"' >> ~/.zshrc
+# ANTHROPIC_BASE_URL="" ANTHROPIC_AUTH_TOKEN="" claude
